b6ab1a9c7185c196e9f586ca307db915aee31b90
gwchicango <yorgech12@gmail.com>
2024-12-22 12:01:39 -0500

    Version 0.3

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -48,7 +48,7 @@ const EpisodeCard = ({ episode }) => {
           onClick={toggleShowMore}
           className="mt-4 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-800 transition duration-300"
         >
-          {showMore ? 'Ver menos' : 'Ver más'}
+          {showMore ? 'Show less' : 'Show more'} 
         </button>
       </div>
 
